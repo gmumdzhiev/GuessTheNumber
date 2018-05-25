c1f2ea6e9762390a43edc75b1240023e84940d17
Georgi Mumdzhiev <george.mumdzhiev@gmail.com>
2018-05-25 10:08:04 +0200

    finishing the store

--- src/store/store.js
+++ src/store/store.js
@@ -2,14 +2,29 @@ import { createStore } from 'redux'
 
 
 const initialState = {
+  done: false,
   guesses: [],
   number: 5,
 }
+export const guess = (number) => ({
+  type: 'GUESS',
+  payload: number,
+})
+
+window.guess = guess
 
 const reducer = (state = initialState, action) => {
-  console.log('action', action)
+
+  if (action.type === 'GUESS') {
+    return {
+      ...state,
+      done: action.payload === state.number,
+      guesses: [action.payload, ...state.guesses],
+    }
+  }
   return state
 }
+
 const store = createStore(reducer)
 
 export default store
